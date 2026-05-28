7741192657e2f2a4ca2b0bd30c6ff5bdcd601bf2
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-28 17:35:24 +0700

    code antrian kosong pengecekan

--- circular.cpp
+++ circular.cpp
@@ -41,3 +41,9 @@ public:
     }
     void remove()
     {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
