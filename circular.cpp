5573adeb29675e567564e9178213ebb2120e54e7
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-28 17:31:56 +0700

    pengecekan atrian kosong

--- circular.cpp
+++ circular.cpp
@@ -22,3 +22,10 @@ public:
         cout << endl;
         // cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+
+            // cek apakah antrian kosong
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
